a464e61cbb6c3e6173f9dfdc2fbbc7a78d44a0c8
mik9016 <mikgru9016@gmail.com>
2024-07-05 23:38:54 +0200

    Add Localization and Description sections

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,7 +14,7 @@ const items = [
 <template>
   <ul class="w-full flex h-24 items-center justify-center gap-8 text-dark">
     <li v-for="item in items">
-      <a :href="`#${item.path}`" class="cursor-pointer hover:underline-offset-8 hover:underline hover:decoration-orange hover:decoration-[8px] ">{{ item.text }}</a>
+      <a :href="`#${item.path}`" class="cursor-pointer hover:underline-offset-8 hover:underline hover:decoration-orange hover:decoration-[8px]">{{ item.text }}</a>
     </li>
   </ul>
  
